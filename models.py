2f4804f609e4befee081f5b8a0cd22d6625288fe
Wendi Ren <47400990+wren44@users.noreply.github.com>
2021-03-24 17:09:56 +0800

    rewrite the graphsage

--- models.py
+++ models.py
@@ -3,12 +3,12 @@ import math
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Linear, Conv1d
-from torch_geometric.nn import GCNConv, RGCNConv, global_sort_pool, global_add_pool,SAGEConv
+from torch_geometric.nn import GCNConv, RGCNConv, global_sort_pool, global_add_pool, SAGEConv
 from torch_geometric.utils import dropout_adj
 from util_functions import *
 import pdb
 import time
-
+import torch.nn.init as init
 
 class GNN(torch.nn.Module):
     # a base GNN class, GCN message passing + sum_pooling
@@ -16,12 +16,12 @@ class GNN(torch.nn.Module):
                  regression=False, adj_dropout=0.2, force_undirected=False):
         super(GNN, self).__init__()
         self.regression = regression
-        self.adj_dropout = adj_dropout 
+        self.adj_dropout = adj_dropout
         self.force_undirected = force_undirected
         self.convs = torch.nn.ModuleList()
         self.convs.append(gconv(dataset.num_features, latent_dim[0]))
-        for i in range(0, len(latent_dim)-1):
-            self.convs.append(gconv(latent_dim[i], latent_dim[i+1]))
+        for i in range(0, len(latent_dim) - 1):
+            self.convs.append(gconv(latent_dim[i], latent_dim[i + 1]))
         self.lin1 = Linear(sum(latent_dim), 128)
         if self.regression:
             self.lin2 = Linear(128, 1)
@@ -38,8 +38,8 @@ class GNN(torch.nn.Module):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         if self.adj_dropout > 0:
             edge_index, edge_type = dropout_adj(
-                edge_index, edge_type, p=self.adj_dropout, 
-                force_undirected=self.force_undirected, num_nodes=len(x), 
+                edge_index, edge_type, p=self.adj_dropout,
+                force_undirected=self.force_undirected, num_nodes=len(x),
                 training=self.training
             )
         concat_states = []
@@ -58,7 +58,145 @@ class GNN(torch.nn.Module):
 
     def __repr__(self):
         return self.__class__.__name__
- 
+
+
+# ------ GraphSAGE start -----------
+class NeighborAggregator(torch.nn.Module):
+    def __init__(self, input_dim, output_dim,
+                 use_bias=False, aggr_method="mean"):
+        """聚合节点邻居
+        Args:
+            input_dim: 输入特征的维度
+            output_dim: 输出特征的维度
+            use_bias: 是否使用偏置 (default: {False})
+            aggr_method: 邻居聚合方式 (default: {mean})
+        """
+        super(NeighborAggregator, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.use_bias = use_bias
+        self.aggr_method = aggr_method
+        self.weight = nn.Parameter(torch.Tensor(input_dim, output_dim))
+        if self.use_bias:
+            self.bias = nn.Parameter(torch.Tensor(self.output_dim))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight)
+        if self.use_bias:
+            init.zeros_(self.bias)
+
+    def forward(self, neighbor_feature):
+        if self.aggr_method == "mean":
+            aggr_neighbor = neighbor_feature.mean(dim=1)
+        elif self.aggr_method == "sum":
+            aggr_neighbor = neighbor_feature.sum(dim=1)
+        elif self.aggr_method == "max":
+            aggr_neighbor = neighbor_feature.max(dim=1)
+        else:
+            raise ValueError("Unknown aggr type, expected sum, max, or mean, but got {}"
+                             .format(self.aggr_method))
+
+        neighbor_hidden = torch.matmul(aggr_neighbor, self.weight)
+        if self.use_bias:
+            neighbor_hidden += self.bias
+
+        return neighbor_hidden
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, aggr_method={}'.format(
+            self.input_dim, self.output_dim, self.aggr_method)
+
+
+class SageGCN(torch.nn.Module):
+    # layer definition
+    def __init__(self, input_dim, hidden_dim,
+                 activation=F.relu,
+                 aggr_neighbor_method="mean",
+                 aggr_hidden_method="sum"):
+        """SageGCN层定义
+        Args:
+            input_dim: 输入特征的维度
+            hidden_dim: 隐层特征的维度，
+                当aggr_hidden_method=sum, 输出维度为hidden_dim
+                当aggr_hidden_method=concat, 输出维度为hidden_dim*2
+            activation: 激活函数
+            aggr_neighbor_method: 邻居特征聚合方法，["mean", "sum", "max"]
+            aggr_hidden_method: 节点特征的更新方法，["sum", "concat"]
+        """
+        super(SageGCN, self).__init__()
+        assert aggr_neighbor_method in ["mean", "sum", "max"]
+        assert aggr_hidden_method in ["sum", "concat"]
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.aggr_neighbor_method = aggr_neighbor_method
+        self.aggr_hidden_method = aggr_hidden_method
+        self.activation = activation
+        self.aggregator = NeighborAggregator(input_dim, hidden_dim,
+                                             aggr_method=aggr_neighbor_method)
+        self.b = nn.Parameter(torch.Tensor(input_dim, hidden_dim))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.b)
+
+    def forward(self, src_node_features, neighbor_node_features):
+        neighbor_hidden = self.aggregator(neighbor_node_features)
+        self_hidden = torch.matmul(src_node_features, self.b)
+
+        if self.aggr_hidden_method == "sum":
+            hidden = self_hidden + neighbor_hidden
+        elif self.aggr_hidden_method == "concat":
+            hidden = torch.cat([self_hidden, neighbor_hidden], dim=1)
+        else:
+            raise ValueError("Expected sum or concat, got {}"
+                             .format(self.aggr_hidden))
+        if self.activation:
+            return self.activation(hidden)
+        else:
+            return hidden
+
+    def extra_repr(self):
+        output_dim = self.hidden_dim if self.aggr_hidden_method == "sum" else self.hidden_dim * 2
+        return 'in_features={}, out_features={}, aggr_hidden_method={}'.format(
+            self.input_dim, output_dim, self.aggr_hidden_method)
+
+
+class GraphSage(torch.nn.Module):
+    # # a base GraphSage class
+    def __init__(self, input_dim, hidden_dim,
+                 num_neighbors_list):
+        super(GraphSage, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_neighbors_list = num_neighbors_list
+        self.num_layers = len(num_neighbors_list)
+        self.gcn = nn.ModuleList()
+        self.gcn.append(SageGCN(input_dim, hidden_dim[0]))
+        for index in range(0, len(hidden_dim) - 2):
+            self.gcn.append(SageGCN(hidden_dim[index], hidden_dim[index + 1]))
+        self.gcn.append(SageGCN(hidden_dim[-2], hidden_dim[-1], activation=None))
+
+    def forward(self, node_features_list):
+        hidden = node_features_list
+        for l in range(self.num_layers):
+            next_hidden = []
+            gcn = self.gcn[l]
+            for hop in range(self.num_layers - l):
+                src_node_features = hidden[hop]
+                src_node_num = len(src_node_features)
+                neighbor_node_features = hidden[hop + 1] \
+                    .view((src_node_num, self.num_neighbors_list[hop], -1))
+                h = gcn(src_node_features, neighbor_node_features)
+                next_hidden.append(h)
+            hidden = next_hidden
+        return hidden[0]
+
+    def extra_repr(self):
+        return 'in_features={}, num_neighbors_list={}'.format(
+            self.input_dim, self.num_neighbors_list
+        )
+
 
 class DGCNN(GNN):
     # DGCNN from [Zhang et al. AAAI 2018], GCN message passing + SortPooling
@@ -69,7 +207,7 @@ class DGCNN(GNN):
         )
         if k < 1:  # transform percentile to number
             node_nums = sorted([g.num_nodes for g in dataset])
-            k = node_nums[int(math.ceil(k * len(node_nums)))-1]
+            k = node_nums[int(math.ceil(k * len(node_nums))) - 1]
             k = max(10, k)  # no smaller than 10
         self.k = int(k)
         print('k used in sortpooling is:', self.k)
@@ -96,8 +234,8 @@ class DGCNN(GNN):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         if self.adj_dropout > 0:
             edge_index, edge_type = dropout_adj(
-                edge_index, edge_type, p=self.adj_dropout, 
-                force_undirected=self.force_undirected, num_nodes=len(x), 
+                edge_index, edge_type, p=self.adj_dropout,
+                force_undirected=self.force_undirected, num_nodes=len(x),
                 training=self.training
             )
         concat_states = []
@@ -123,28 +261,28 @@ class DGCNN(GNN):
 class DGCNN_RS(DGCNN):
     # A DGCNN model using RGCN convolution to take consideration of edge types.
     def __init__(self, dataset, gconv=RGCNConv, latent_dim=[32, 32, 32, 1], k=30,
-                 num_relations=5, num_bases=2, regression=False, adj_dropout=0.2, 
+                 num_relations=5, num_bases=2, regression=False, adj_dropout=0.2,
                  force_undirected=False):
         super(DGCNN_RS, self).__init__(
-            dataset, 
+            dataset,
             SAGEConv,
-            latent_dim, 
-            k, 
-            regression, 
-            adj_dropout=adj_dropout, 
+            latent_dim,
+            k,
+            regression,
+            adj_dropout=adj_dropout,
             force_undirected=force_undirected
         )
         self.convs = torch.nn.ModuleList()
         self.convs.append(gconv(dataset.num_features, latent_dim[0], num_relations, num_bases))
-        for i in range(0, len(latent_dim)-1):
-            self.convs.append(gconv(latent_dim[i], latent_dim[i+1], num_relations, num_bases))
+        for i in range(0, len(latent_dim) - 1):
+            self.convs.append(gconv(latent_dim[i], latent_dim[i + 1], num_relations, num_bases))
 
     def forward(self, data):
         x, edge_index, edge_type, batch = data.x, data.edge_index, data.edge_type, data.batch
         if self.adj_dropout > 0:
             edge_index, edge_type = dropout_adj(
-                edge_index, edge_type, p=self.adj_dropout, 
-                force_undirected=self.force_undirected, num_nodes=len(x), 
+                edge_index, edge_type, p=self.adj_dropout,
+                force_undirected=self.force_undirected, num_nodes=len(x),
                 training=self.training
             )
         concat_states = []
@@ -167,33 +305,34 @@ class DGCNN_RS(DGCNN):
             return F.log_softmax(x, dim=-1)
 
 
-class IGMC(GNN):
+class IGMC(GraphSage):
     # The GNN model of Inductive Graph-based Matrix Completion. 
     # Use RGCN convolution + center-nodes readout.
-    def __init__(self, dataset, gconv=RGCNConv, latent_dim=[32, 32, 32, 32], 
-                 num_relations=5, num_bases=2, regression=False, adj_dropout=0.2, 
-                 force_undirected=False, side_features=False, n_side_features=0, 
+    def __init__(self, dataset, gconv=RGCNConv, latent_dim=[32, 32, 32, 32],
+                 num_relations=5, num_bases=2, regression=False, adj_dropout=0.2,
+                 force_undirected=False, side_features=False, n_side_features=0,
                  multiply_by=1):
         super(IGMC, self).__init__(
-            dataset, SAGEConv, latent_dim, regression, adj_dropout, force_undirected
+            dataset, GraphSage, latent_dim
         )
+        # delete：, regression, adj_dropout, force_undirected (no such parameters in GraphSage)
         self.multiply_by = multiply_by
         self.convs = torch.nn.ModuleList()
         self.convs.append(gconv(dataset.num_features, latent_dim[0], num_relations, num_bases))
-        for i in range(0, len(latent_dim)-1):
-            self.convs.append(gconv(latent_dim[i], latent_dim[i+1], num_relations, num_bases))
-        self.lin1 = Linear(2*sum(latent_dim), 128)
+        for i in range(0, len(latent_dim) - 1):
+            self.convs.append(gconv(latent_dim[i], latent_dim[i + 1], num_relations, num_bases))
+        self.lin1 = Linear(2 * sum(latent_dim), 128)
         self.side_features = side_features
         if side_features:
-            self.lin1 = Linear(2*sum(latent_dim)+n_side_features, 128)
+            self.lin1 = Linear(2 * sum(latent_dim) + n_side_features, 128)
 
     def forward(self, data):
         start = time.time()
         x, edge_index, edge_type, batch = data.x, data.edge_index, data.edge_type, data.batch
         if self.adj_dropout > 0:
             edge_index, edge_type = dropout_adj(
-                edge_index, edge_type, p=self.adj_dropout, 
-                force_undirected=self.force_undirected, num_nodes=len(x), 
+                edge_index, edge_type, p=self.adj_dropout,
+                force_undirected=self.force_undirected, num_nodes=len(x),
                 training=self.training
             )
         concat_states = []
